78cc6eb235d6a7570e4a2f63cfcd5ed0538463fb
Syeong <pyqw_@naver.com>
2024-11-12 21:39:06 +0900

    feat: implement delete account page

--- src/components/MyPage/MyPageMenu.jsx
+++ src/components/MyPage/MyPageMenu.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../../styles/MyPage/MyPageMenu.css'
 
 const MyPageMenu = () => {
     const navigate = useNavigate();
@@ -18,11 +19,14 @@ const MyPageMenu = () => {
 
     return (
         <div className='myPageMenu'>
+            <h2>MY PAGE</h2>
+            <hr></hr>
             <ul className="nav-myPageMenu">
                 <li className="nav-editProfile" onClick={moveToEditProfile}>회원 정보 수정</li>
                 <li className="nav-myCertificate" onClick={moveToMyCertificate}>취득 자격증 관리</li>
                 <li className="nav-deleteAccount" onClick={moveToDeleteAccount}>회원 탈퇴</li>
             </ul>
+            <hr></hr>
         </div>
     );
 };
