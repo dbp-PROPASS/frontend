f5567d498cb49bd817ba19872cc1db00a8a30ab3
Syeong <pyqw_@naver.com>
2024-11-12 11:25:50 +0900

    feat: Initialize routing setup
    - Add BrowserRouter setup in index.js
    - Create basic structure for App.js
    - Add basic App.css file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar'
+import AppRoutes from './routes/routes'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Navbar/>
+      <AppRoutes/>
     </div>
   );
 }
